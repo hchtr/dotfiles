29c83017374044b5b2a81c61d6949e8de2642dc8
hchtr <155396759+hchtr@users.noreply.github.com>
2025-10-29 22:35:10 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -67,7 +67,7 @@ for file in "$DOTFILES_DIR/.config/i3status/"*; do
   ln -s "$file" "$HOME/.config/i3status/$filename"
 done
 
-for file in .fehbg .tmux.conf .vimrc .xinitrc .Xresources; do
+for file in .fehbg .tmux.conf .xinitrc .Xresources; do
   rm -f "$HOME/$file"
   ln -s "$DOTFILES_DIR/$file" "$HOME/$file"
 done
