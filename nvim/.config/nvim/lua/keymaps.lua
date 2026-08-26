8245dc0b10f45e2c059ba1eb07cc4193aa2bd25f
hchtr <155396759+hchtr@users.noreply.github.com>
2026-08-26 17:42:13 +0100

    Update config

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,7 +1,3 @@
-vim.api.nvim_set_keymap(  'i'  ,  '('  , '()<Esc>i'  ,   { noremap = true, silent = true }  )
-vim.api.nvim_set_keymap(  'i'  ,  '['  , '[]<Esc>i'  ,   { noremap = true, silent = true }  )
-vim.api.nvim_set_keymap(  'i'  ,  '{'  , '{}<Esc>i'  ,   { noremap = true, silent = true }  )
-vim.api.nvim_set_keymap(  'i'  ,  '"'  , '""<Esc>i'  ,   { noremap = true, silent = true }  )
 --
 vim.api.nvim_set_keymap(  't'  ,  '<ESC>'  ,  '<C-\\><C-n>'  ,  {noremap = true}  )
 --
